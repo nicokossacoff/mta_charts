dd4bd5d52ea7633f41b5fbb1613279deccc8fc60
Nicolás Kossacoff <nicokossacoff@gmail.com>
2024-03-04 08:50:19 -0300

    Included the setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+
+setup(name= 'mta_charts',
+      version= '1.0.0',
+      description= 'Charts for MTA modelling',
+      author= 'Nicolás Kossacoff',
+      author_email= 'nicokossacoff@gmail.com',
+      license= 'MIT',
+      packages= setuptools.find_packages(),
+      classifiers= ["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License"],
+      zip_safe= False)
